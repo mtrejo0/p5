008f6d3f841e5ed4924921782a68562378c58041
Moises Trejo <moises.trejo0@gmail.com>
2025-10-21 17:43:13 +0200

    cors

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ import os
 
 PORT = int(os.environ.get('PORT', 8000))
 
-Handler = http.server.SimpleHTTPRequestHandler
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super().end_headers()
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), CORSRequestHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
